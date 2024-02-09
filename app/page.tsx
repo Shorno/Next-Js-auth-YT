d6977e0b2c43e5d548c3ce941ecb07adaed46d6a
shorno <fb.shorno@gmail.com>
2024-02-09 13:14:45 +0600

    Code from YT

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import {UserButton} from "@clerk/nextjs";
+
+export default function Home() {
+    return (
+        <>
+            <div className="absolute to-0 right-0 px-10">
+            </div>
+            <div>
+                <h1 className="mt-10 text-center">Home | Testing Auth</h1>
+            </div>
+
+        </>
+
+    );
+}
